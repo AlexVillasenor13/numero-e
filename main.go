5aa24f3737ef56a0f6acf4a730393fc3ec5962f7
AlexVillasenor13 <isaac.villasenor2921@alumnos.udg.mx>
2020-09-26 17:14:32 -0500

    Programa de número e

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n float64
+	var e float64 = 0
+	fmt.Scan(&n)
+	for i:=0.0; i<n; i++{
+		fact := 1.0
+		for j:=1.0; j<=i; j++{
+			fact *= j
+		}
+		e += 1.0 /fact
+	}
+
+	fmt.Print(e)
+}
